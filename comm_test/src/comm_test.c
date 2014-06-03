513d88370f6e1c8fc2e6f86b56d46a959bf33945
cai-loc <cai-loc@tardis-stu1.ecs.baylor.edu>
2014-06-03 13:32:27 -0500

    MPI: MPI_Send does not have to wait for MPI_Recv. Sent to destination doesnt not mean processed by MPI_Recv.

--- comm_test/src/comm_test.c
+++ comm_test/src/comm_test.c
@@ -1,6 +1,8 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 void schedule(const int id, const int p) 
 {
@@ -22,8 +24,14 @@ int monitor(const int id, const int p)
     MPI_Status status;
     printf("----------> This is root node, id = %d\n", id);
     for (i = 1; i < p; i++) {
-        MPI_Recv(&data, 1, MPI_INT, i, i, MPI_COMM_WORLD, &status);
-        printf("\n ----> root node: data recieved from node %d, data = %d\n\n", i, data);
+        if (i == 5) {
+            puts("\nzzzzzzzzzzzzz delaying node 0 ... \n");
+            sleep(5);
+            puts("\nooooooooooooo node 0 woke up\n");
+        }
+        
+        /* MPI_Recv(&data, 1, MPI_INT, i, i, MPI_COMM_WORLD, &status); */
+        /* printf("\n >>>>>>>>>> root node: data recieved from node %d, data = %d\n\n", i, data); */
     }
     return id;
 }
@@ -31,8 +39,9 @@ int monitor(const int id, const int p)
 int worker(const int id)
 {
     int data = id;
-    printf("= = = = = =>This is child node, id = %d\n", id);
+    /* printf("= = = = = =>This is child node, id = %d\n", id); */
     MPI_Send(&data, 1, MPI_INT, 0, id, MPI_COMM_WORLD);
+    printf("\n<<<<<<<< node %d: data sent\n\n", id);
     return id;
 }
 
@@ -41,16 +50,23 @@ int main(int argc, char *argv[])
     int id = -1; /* uninitialized */
     int p  = -1; /* uninitialized */
     printf("begin: id = %d, p = %d\n", id, p);
-
+    clock_t start = clock(), diff;
+    
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
+    
+    diff = clock() - start;
+    double msc = diff * 1000/CLOCKS_PER_SEC;
+    if (id == 0) {
+        printf(" ******* Initialization too %f ms *******\n", diff);
+    }
     
     printf("after init: id = %d, p %d \n", id, p);
     
     schedule(id, p);
     
-    MPI_Finalize();
     printf("done: id = %d\n", id);
+    MPI_Finalize();
     return 0;
 }
