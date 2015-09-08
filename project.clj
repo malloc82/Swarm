8041a52e4a4311632616f53956e515bae77636b2
Ritchie Cai <ritchiecai@r2labs.org>
2015-09-08 00:53:34 -0500

    Project start

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject swarm "0.0.1"
+  :description "Particle Swarm Optimization & Superiorization"
+  ;; :url "http://example.com/FIXME"
+  ;; :license {:name "Eclipse Public License"
+  ;;           :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [net.mikera/core.matrix "0.40.0"]
+                 [incanter/incanter-charts "1.5.6"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
