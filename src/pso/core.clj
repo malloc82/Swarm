ecaa2fc846e9f3e7cf553bd0e86e7279251d6eeb
Ritchie Cai <ritchiecai@r2labs.org>
2015-09-08 01:38:41 -0500

    start pso search function

--- src/pso/core.clj
+++ src/pso/core.clj
@@ -1,6 +1,24 @@
-(ns pso.core)
+(ns pso.core
+  (:use func.core
+        [clojure.pprint :only [pprint]])
+  (:require [clojure.core.matrix :as mat]
+            [clojure.core.matrix.operators :as op]
+            ;; [clojure.core.async :refer [chan buffer go go-loop timeout >! >!! <! <!! close! alt!]]
+            ))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(set! *warn-on-reflection* true)
+
+(mat/set-current-implementation :vectorz)
+
+(defn search
+  "Particle swarm search:
+  update scheme:
+      x(k + 1) = x(k) + v(k)
+      v(k + 1) = w.v(k) +
+                 r(0, a_1).(x_localbest(k) - x(k)) +
+                 r(0, a_2).(x_globalbest(k) - x(k))
+  w: should between [0.9, 1.2]"
+  [func & {:keys [agent-count search-range]
+           :or   {agent-count 100
+                  search-range [-10 10]}}]
+  )
