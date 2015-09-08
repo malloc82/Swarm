58444c651b83368178a4140bc44e1156789851ed
Ritchie Cai <ritchiecai@r2labs.org>
2015-09-08 01:45:10 -0500

    more project.clj update

--- project.clj
+++ project.clj
@@ -5,5 +5,12 @@
   ;;           :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [net.mikera/core.matrix "0.40.0"]
+                 [net.mikera/vectorz     "0.50.0"]
+                 [clatrix "0.5.0"]
                  [incanter/incanter-charts "1.5.6"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+  ;; :jvm-opts ^:replace ["-server" "-XX:+UseCompressedOops" "-XX:+AggressiveOpts"
+  ;;                      "-XX:+UnlockCommercialFeatures"
+  ;;                      "-XX:+FlightRecorder"]
+  :jvm-opts ^:replace []
+)
